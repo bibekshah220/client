a7a0218fdfcef7801593e3742307732809f54bbb
bibekshah220 <bibekshah425@gmail.com>
2025-10-12 19:22:19 +0545

    routing and form handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-
-import './App.css'
-import LoginPage from './pages/auth/login'
-import RegisterPage from './pages/auth/register'
-
+import "./App.css";
+import LoginPage from "./pages/auth/login";
+import RegisterPage from "./pages/auth/register";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
-      <LoginPage />
-      <RegisterPage/>
+      <BrowserRouter>
+        <Routes>
+          {/* defining routes */}
+          <Route path="/sign-in" element={<LoginPage />} />
+          <Route path="/sign-up" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/common/ui/inputs/input.tsx
+++ src/components/common/ui/inputs/input.tsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { LuAsterisk } from 'react-icons/lu'
+import React from "react";
+import { LuAsterisk } from "react-icons/lu";
 
 interface IProps {
-    label: string;
-    id: string;
-    placeholder?: string
-    type?: "text" | 'email' | 'password' | 'date';
-
-
+  label: string;
+  id: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "date";
+  onChange?: (e: any) => void;
 }
 
-const Input:React.FC<IProps> = ({id,label,type = 'text',placeholder='Enter something '}) => {
-    return (
-        <div>
-            <div className="flex">
-                <label className="text-[16px] text-gray-700 font-semibold" htmlFor={id}>{label}</label>
-                <LuAsterisk className="text-red-500" />
-            </div>
-            <input
-                className="focus:outline-blue-500 mt-1 w-full py-3 px-2 border  rounded-md border-gray-300 text-[16px] placeholder:text-[16px] placeholder:text-gray-400"
-                placeholder={placeholder}
-                type={type}
-                id={id}
-            />
-        </div>
-    )
-}
+const Input: React.FC<IProps> = ({
+  id,
+  label,
+  type = "text",
+  placeholder = "Enter something ",
+}) => {
+  return (
+    <div>
+      <div className="flex">
+        <label className="text-[16px] text-gray-700 font-semibold" htmlFor={id}>
+          {label}
+        </label>
+        <LuAsterisk className="text-red-500" />
+      </div>
+      <input
+        className="focus:outline-blue-500 mt-1 w-full py-3 px-2 border  rounded-md border-gray-300 text-[16px] placeholder:text-[16px] placeholder:text-gray-400"
+        placeholder={placeholder}
+        type={type}
+        id={id}
+      />
+    </div>
+  );
+};
 
-export default Input
+export default Input;
